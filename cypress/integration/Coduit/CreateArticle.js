e1d580008e9baa31a63c53a4d515825cd5f40605
Adam Juraj Tomasov <adamjt@MacBook-Pro.tcdefault>
2022-03-23 21:37:36 +0100

    small updates

--- cypress/integration/Coduit/CreateArticle.js
+++ cypress/integration/Coduit/CreateArticle.js
@@ -5,31 +5,34 @@ import articleData from '../../fixtures/article.json'
 const articlePage = new ArticlePage();
 const uuid = () => Cypress._.random(0, 1e2)
 const id = uuid()
-const testname = `testname${id}`
+const testname = `testname${id}`;
 
 describe(`Create and Publish Article Test`, () => {
 
     beforeEach('Log in user', () => {
+       
         cy.intercept('GET', '/api/tags').as('tags');
         cy.visit('https://react-redux.realworld.io/#/login?_k=z8eifk');
-        cy.SignIn(userData.users[0].email, userData.users[0].pwd);
     })
 
-    articleData.articles.forEach(element => {
-        it(`Create article`, () => {
-            cy.wait('@tags').then(() => {
-                cy.get('.container .nav :nth-child(2) .nav-link').eq(0).click();
+    userData.users.forEach(user => {
+        articleData.articles.forEach(element => {
+            it(`Create article for user ` + user.email, () => {
+                cy.SignIn(user.email, user.pwd);
                 cy.wait('@tags').then(() => {
-                    articlePage.getTitleInput().type(element.title + testname);
-                    articlePage.getAboutInput().type(element.about);
-                    articlePage.getTextArea().type(element.text);
-                    articlePage.getTagInput().type(element.tag);
-                    articlePage.getPublishButton().click();
+                    cy.get('.container .nav :nth-child(2) .nav-link').eq(0).click();
+                    cy.wait('@tags').then(() => {
+                        articlePage.getTitleInput().type(element.title + ' ' + testname);
+                        articlePage.getAboutInput().type(element.about);
+                        articlePage.getTextArea().type(element.text);
+                        articlePage.getTagInput().type(element.tag);
+                        articlePage.getPublishButton().click();
 
-                    cy.get('h1').should('contain.text', element.title);
-                    cy.get('.author').should('contain.text', userData.users[0].email);
-                    cy.get('body').should('be.visible');
-                    articlePage.getDeleteButton().click();
+                        cy.get('h1').should('contain.text', element.title);
+                        cy.get('.author').should('contain.text', user.email);
+                        cy.get('body').should('be.visible');
+                        articlePage.getDeleteButton().click();
+                    })
                 })
             })
         })
