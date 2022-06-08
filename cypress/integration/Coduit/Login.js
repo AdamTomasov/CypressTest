97640670c9b9029a816f6da4676375d8005a2dec
Adam Juraj Tomasov <adamjt@MacBook-Pro.tcdefault>
2022-06-08 12:24:45 +0200

    small update

--- cypress/integration/Coduit/Login.js
+++ cypress/integration/Coduit/Login.js
@@ -3,7 +3,8 @@ import data from '../../fixtures/users.json'
 describe("Login test", () => {
     data.users.forEach(element => {
         it(`Succes login: ` + element.email, () => {
-            cy.visit('https://react-redux.realworld.io/#/login?_k=z8eifk');
+            cy.setCookie('token', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3R1c2VyQGlwdGlxLmNvbSIsInVzZXJuYW1lIjoidGVzdHVzZXJAaXB0aXEuY29tIiwiaWF0IjoxNjU0MDk2Mjk3LCJleHAiOjE2NTkyODAyOTd9.woYaAdZomm-wq-cSlHB80sCtHBu_YCDSsLGhMes-KpM")
+            cy.visit('https://react-redux.realworld.io/#editor?_k=p64g3v');
             cy.SignIn(element.email, element.pwd);
             cy.get(':nth-child(4) a.nav-link').should('contains.text', element.email);
         });
