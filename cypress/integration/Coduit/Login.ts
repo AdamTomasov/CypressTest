2abb84d6d08d85c291d10f8c84c3eba9696b5936
Adam Juraj Tomasov <adamjt@MacBook-Pro.tcdefault>
2022-11-02 17:13:21 +0100

    test'

--- cypress/integration/Coduit/Login.ts
+++ cypress/integration/Coduit/Login.ts
@@ -6,7 +6,7 @@ describe("Login test", () => {
             cy.setCookie('token', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3R1c2VyQGlwdGlxLmNvbSIsInVzZXJuYW1lIjoidGVzdHVzZXJAaXB0aXEuY29tIiwiaWF0IjoxNjU0MDk2Mjk3LCJleHAiOjE2NTkyODAyOTd9.woYaAdZomm-wq-cSlHB80sCtHBu_YCDSsLGhMes-KpM")
             cy.visit('https://react-redux.realworld.io/#editor?_k=p64g3v');
             cy.SignIn(element.email, element.pwd);
-            cy.get(':nth-child(4) a.nav-link').should('contains.text', element.email);
+            cy.get(':nth-child(4) a.nav-link').should('contains.text', element.email); //aa
         });
     });
 });
